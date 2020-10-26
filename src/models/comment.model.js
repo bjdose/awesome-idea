8416512b7c5f166229b0d8f8f50e70a39477e234
Brayan <16-11087@usb.ve>
2020-10-26 00:21:16 -0600

    add: models added

---
+++ src/models/comment.model.js
@@ -0,0 +1,25 @@
+const { Mongoose } = require('mongoose');
+
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const CommentSchema = new Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+    autopopulate: true,
+  },
+});
+
+CommentSchema.plugin(require('mongoose-autopopulate'));
+
+module.exports = mongoose.model('comment', CommentSchema);
