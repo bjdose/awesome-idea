8a71f4759c9f18767a106a6c464dfe9d658ded4f
Brayan <16-11087@usb.ve>
2020-10-25 20:53:21 -0600

    add: first steps added

---
+++ src/routes/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+require('express-async-errors');
+
+const {
+  ErrorMiddleware,
+  NotFoundMiddleWare,
+} = require('../middlewares');
+const { Router } = require('express');
+
+module.exports = function ({ HomeRoutes }) {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+
+  apiRoutes
+    .use(express.json())
+    .use(cors())
+    .use(helmet())
+    .use(compression());
+
+  apiRoutes.use('/home', HomeRoutes);
+
+  router.use('/v1/api', apiRoutes);
+
+  router.use(NotFoundMiddleWare);
+  router.use(ErrorMiddleware);
+
+  return router;
+};
