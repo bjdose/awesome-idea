ad308845283fbdf4dacf7096ad4d8a64b4afe8ac
Brayan <16-11087@usb.ve>
2020-11-03 21:54:03 -0600

    add: tests added

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: '5f9baa5fc66a6b9880d92092',
+    name: 'Brayan Salcedo Testing 1',
+    username: 'bjdose 1',
+    password: 'asdjsadkla',
+  },
+  users: [
+    {
+      _id: '5f9baa5fc66a6b9880d92048',
+      name: 'Brayan Salcedo Testing 2',
+      username: 'bjdose 2',
+      password: 'sdsfa@#ASDd',
+    },
+    {
+      _id: '5f9baa5fc66a6b9580d92098',
+      name: 'Brayan Salcedo Testing 3',
+      username: 'bjdose 3',
+      password: 'ksLD12L@3%%!',
+    },
+  ],
+};
